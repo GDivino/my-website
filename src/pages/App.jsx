b34f00034604adada3ec8356db71c1f035a93982
GDivino <giodivino.tech@gmail.com>
2022-01-02 14:29:16 +0800

    Refactor boilerplate react code

---
+++ src/pages/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import { index as Landing } from '../pages/landing'
+import '../styles/App.scss'
+
+const App = () => {
+  <Landing />
+}
+
+export default App
---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,7 @@
+import '../../styles/index.scss'
+
+const index = () => {
+    <iframe src='https://my.spline.design/giodj-0b95bb3212d71b4e311b35664e5b90a8/' frameborder='0' width='100%' height='100%'></iframe>
+}
+
+export default index
