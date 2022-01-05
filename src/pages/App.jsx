0919c46e7dd715895d5313c292da2318089f7661
GDivino <giodivino.tech@gmail.com>
2022-01-05 14:28:35 +0800

    fix imports

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
-import { default as Landing } from '../pages/landing/index'
-import { default as Navigation } from '../pages/navigation/index'
+import Landing  from '../pages/landing/index'
+import Navigation from '../pages/navigation/index'
 
 const App = () => {
   return (
