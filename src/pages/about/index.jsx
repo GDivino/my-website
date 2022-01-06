4aaa86f3a8a9fa1aad66250f842eacce2b0fa175
GDivino <giodivino.tech@gmail.com>
2022-01-06 20:13:28 +0800

    add about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,41 @@
+import '../../styles/about.scss'
+import DisplayPic from '../../static/about/Idpic_final.png'
+
+const About = () => {
+    return (
+        <div id='about'>
+            <div className='container'>
+                <img src={DisplayPic} className='about-img' />
+                <div className='about-details'>
+                    <div className='p1'>
+                        <h2>About Me</h2>
+                        <p>Hey there, I'm Gio! I am a <a>Fullstack Software Engineer</a> that loves building things and learning new technologies. My tech journey started last August, 2020, just when the pandemic struck the world. I learned how to program, and built all sorts of things, and it was awesome!</p>
+                        <br />
+                        <p>As of today, I've grown my experience by working in <a>edtech</a>, <a>healthtech, a Y-Combinator Startup</a>, <a> a student-led developer organization</a>, and maybe even you 😉!</p>
+                        <br />
+                        <p>I hope to continue making an impact through tech by building and learning!</p>
+                        <br />
+                        <p>Here are some technologies I've worked with recently:</p>
+                        <br />
+                    </div>
+                    <div className='p2'>
+                        <ul>
+                            <li>ReactJS</li>
+                            <li>NodeJS</li>
+                            <li>MongoDB</li>
+                            <li>Typescript</li>
+                        </ul>
+                        <ul>
+                            <li>Jest</li>
+                            <li>Django</li>
+                            <li>MySQL</li>
+                            <li>Docker</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
