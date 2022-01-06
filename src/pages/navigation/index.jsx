58f3ee0a8ffca8e8b940104a55a56f50e86a154f
GDivino <giodivino.tech@gmail.com>
2022-01-06 14:45:03 +0800

    create responsive navbar

--- src/pages/navigation/index.jsx
+++ src/pages/navigation/index.jsx
@@ -1,41 +1,87 @@
+import { useState, useEffect } from 'react'
 import '../../styles/navigation.scss'
 import { NavLink } from "react-router-dom"
 
-const index = () => {
+const Burger = () => {
     return (
-        <div id='navigation'>
-            <nav className='navbar'>
-                <ul>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='/about'>
-                            About
-                        </NavLink>
-                    </li>
-                </ul>
-                <ul>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='/experience'>
-                            Experience
-                        </NavLink>
-                    </li>
-                </ul>
-                <ul>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='/projects'>
-                            Projects
-                        </NavLink>
-                    </li>
-                </ul>
-                <ul>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='/contact'>
-                            Contact
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
+        <div className='burger'>
+            <div></div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
 
-export default index
+const Exit = () => {
+    return (
+        <div className='exit'>
+            <div id='mdiv'>
+                <div className='mdiv'>
+                    <div className='md'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Navigation = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    }, [])
+
+    return (
+        <div id='navigation'>
+            {(screenWidth > 700 || toggleMenu) && 
+                (<nav className='navbar'>
+                    <ul>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/about' onClick={toggleNav}>
+                                About
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/experience' onClick={toggleNav}>
+                                Experience
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/projects' onClick={toggleNav}>
+                                Projects
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='/contact' onClick={toggleNav}>
+                                Contact
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>)
+            }
+            {!toggleMenu && (<a onClick={toggleNav}><Burger className='burger' /></a>)}
+            {toggleMenu && (<a onClick={toggleNav}><Exit className='exit'/></a>)}
+        </div>
+    )
+}
+
+export default Navigation
