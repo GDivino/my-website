557912f625575b4c1c937bd5e5f8ebcc7b11a5bf
GDivino <giodivino.tech@gmail.com>
2022-03-22 23:49:08 +0800

    fix typo

--- src/components/experience/Abakada.jsx
+++ src/components/experience/Abakada.jsx
@@ -3,7 +3,7 @@ const Abakada = () => {
         <div data-aos='fade-left' className='career-info'>
             <div className='container'>
                 <h3>Web Developer @ <a href='https://abakadastudios.com' target='_blank'>Abakada Studios</a></h3>
-                <p className='date'>Aptil 2021 - July 2021</p>
+                <p className='date'>April 2021 - July 2021</p>
                 <p>Developed integration scripts for Enterprise Resource Planning (ERP) systems and Learning Management Systems (LMS) using Javascript</p>
             </div>
         </div>
