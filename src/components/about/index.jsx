f25b0e9f780870b3c0ccaabd0280dd74ac82ce38
GDivino <giodivino.tech@gmail.com>
2022-12-20 01:44:38 +0800

    added lazy loading and some content changes

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -8,14 +8,15 @@ const About = () => {
                 <div className='about-details'>
                     <div className='p1'>
                         <h2>About Me</h2>
-                        <p>Hey there, I'm Gio! I am a <a>DevOps Engineer</a> that loves building things and learning new technologies. My tech journey started last August, 2020, just when the pandemic struck the world. I learned how to program, and built all sorts of things, and it was awesome!</p>
-                        <br />
-                        <p>As of today, I've grown my experience by working in <a>edtech, </a> <a>healthtech, and fintech companies, 2 Y-Combinator Companies</a>, <a> a student-led developer organization, and maybe even for you</a> 😉!</p>
-                        <br />
+                        <p>Hey there, I'm Gio! I am a DevOps Engineer that loves building things and learning new technologies.</p>
+                            
+                        <p> My tech journey started last August, 2020, just when the pandemic struck the world. I learned how to program, and built all sorts of things, and it was awesome! </p>
+                        
+                        <p>As of today, I've grown my experience by working in <a href='https://abakadastudios.com' target='_blank'>edtech</a>, <a href='https://dashlabs.ai' target='_blank'>healthtech</a>, and <a href='https://xendit.co' target='_blank'>fintech companies</a>, <a href='https://www.ycombinator.com/' target='_blank'>2 Y-Combinator Companies</a>, <a href='https://gdscloyola.org' target='_blank'> a student-led developer organization</a>, and maybe even for you 😉!</p>
+                        
                         <p>I hope to continue making an impact through tech by building and learning!</p>
-                        <br />
+
                         <p>Here are some technologies I've worked with recently:</p>
-                        <br />
                     </div>
                     <div className='p2'>
                         <ul>
--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,14 +1,16 @@
+import React, { lazy, Suspense } from 'react'
 import { useEffect, useState } from 'react'
 import '../../styles/landing.scss'
-import Face from './face/Face'
 import { Link } from 'react-scroll'
+
+const Face = lazy(() => import('./face/Face'))
 
 const Landing = () => {   
     const [width, setWidth] = useState(window.innerWidth)
 
     useEffect(() => {
         const changeWidth = () => {
-            setWidth(width.innerWidth)
+            setWidth(window.innerWidth)
         }
 
         window.addEventListener('resize', changeWidth)
@@ -16,12 +18,17 @@ const Landing = () => {
         return () => {
             window.removeEventListener('resize', changeWidth)
         }
-    }, [])
+        
+    }, [width])
     
     return (
         <div id='landing'>
             <div className='container'>
-                {width > 700 && <Face />}
+                {width > 700 && 
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Face />
+                    </Suspense>
+                }
                 <Link 
                     to='contact'
                     smooth={true}
@@ -33,7 +40,7 @@ const Landing = () => {
                 <div className='landing-info'>
                     <p className='greeting'>Hi my name is</p>
                     <h1 className='name'>Gio Divino.</h1>
-                    <h4 className='short-desc'>I build smooth and healthy IT Infra</h4>
+                    <h4 className='short-desc'>I make Software Delivery a breeze</h4>
                     <p className='long-desc'>I am a Manila-based DevOps Engineer that creates smooth operations, and builds healthy IT infrastructure. Currently, I am looking for DevOps opportunities to grow as a complete engineer.</p>
                 </div>
             </div>
