258e71596b2e2c88184e15196046924458ffd898
GDivino <giodivino.tech@gmail.com>
2023-05-08 20:56:08 +0800

    change landing desc

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -38,7 +38,7 @@ const Landing = () => {
                     <p className='greeting'>Hi my name is</p>
                     <h1 className='name'>Gio Divino.</h1>
                     <h4 className='short-desc'>I make Software Delivery a breeze</h4>
-                    <p className='long-desc'>I am a Manila-based DevOps Engineer that creates smooth operations, and builds healthy IT infrastructure. Currently, I am looking for DevOps opportunities to grow as a complete engineer.</p>
+                    <p className='long-desc'>I am a Manila-based DevOps Engineer that creates smooth operations, and builds healthy IT infrastructure. Currently, I am looking to help companies produce breakthrough innovations through DevOps.</p>
                 </div>
             </div>
         </div>
