1d254ecfcdbbb23539ed063da5c79ba4b37d7b76
GDivino <giodivino.tech@gmail.com>
2024-01-26 00:50:03 +0800

    feat: revamp and update relevant work experiences

--- src/components/experience/ExperienceObjects.js
+++ src/components/experience/ExperienceObjects.js
@@ -5,13 +5,29 @@ export const Companies = [
         "link": "https://xendit.co",
         "card": [
             {
+              "title": "Infrastructure Engineer",
+              "date": "July 2023 - January 2024",
+              "desc": "Provided infrastructure support to product and initiative teams from infrastructure design and implementation to infrastructure maintenance, ensuring an ecosystem of reliable and scalable infrastructure, backing up an SLA of 99.99%."
+            },
+            {
                 "title": "Infrastructure Engineer Intern",
                 "date": "June 2022 - September 2022",
-                "desc": "Managed AWS resources for developers using terraform IaC, and supported infrastructure team with research on aggregated SSL certificate management and mirroring public container images into AWS ECR."
-            }
+                "desc": "Supported product teams by efficiently managing infrastructure service desk tickets, encompassing tasks such as provisioning resources with terraform, managing AWS IAM access for developers, cleaning up leftover toil, and optimizing infrastructure operations"
+            },
         ]
     },
-
+    {
+        "company": "Bonito Technologies",
+        "short_name": "Bonito Tech",
+        "link": "https://bonitotech.com",
+        "card": [
+            {
+                "title": "DevOps Engineer",
+                "date": "January 2023 - April 2023",
+                "desc": "Optimized the company's internal tooling and deployment processes by creating cost comparisons and systematizing deployment strategies for internal tools; simultaneously enhancing deployment efficiency through a CLI tool, and modernizing legacy terraform code."
+            },
+        ]
+    },
     {
         "company": "Dashlabs.ai (YC W21)",
         "short_name": "Dashlabs.ai",
@@ -20,16 +36,15 @@ export const Companies = [
             {
                 "title": "Software Engineer",
                 "date": "August 2021 - November 2021",
-                "desc": "Ensured high quality of products by writing software tests with Jest, and fixed bugs in code."
+                "desc": "Coded unit tests and troubleshot frontend logic, increasing code coverage and enhancing code quality by 23%; and concurrently created automations for business teams and optimizing manual, yet important processes."
             },
             {
                 "title": "Client Success Intern",
                 "date": "July 2021 - August 2021",
-                "desc": "Provided Account Management support to clients to ensure satisfaction with the product."
+                "desc": "Supported clients by troubleshooting product issues, retrieving relevant data, and automating manual processes."
             }
         ]
     },
-
     {
         "company": "Abakada Studios",
         "short_name": "Abakada Studios",
@@ -42,7 +57,6 @@ export const Companies = [
             }
         ]
     },
-
     {
         "company": "Google Developer Student Clubs Loyola",
         "short_name": "GDSC-L",
@@ -59,5 +73,5 @@ export const Companies = [
                 "desc": "Developed the products of the organization in the fields of cloud hosting, data integration, and frontend and backend integration using tools such as heroku, python, firebase, and flask"
             }
         ]
-    }
+    },
 ]
